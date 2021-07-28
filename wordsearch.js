efc5aba456623d3cd6288aa866c8e11402fc6634
gurpreet singh <gurpreet212004@gmail.com>
2021-07-28 20:38:25 +0000

    done with horizontal and vertical checks

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,29 @@
-const wordSearch = (letters, word) => { 
+const wordSearch = (letters, word) => {
     const horizontalJoin = letters.map(ls => ls.join(''))
+    const horizontalJoinReverse = letters.map(ls => ls.reverse().join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+    for (l of horizontalJoinReverse) {
+        if (l.includes(word)) return true
+    }
+    const verticalJoin = [];
+    const verticalJoinReverse = [];
+    for (let i = 0; i < letters[0].length; i++) {
+        const newRow = [];
+        for (row of letters) {
+            newRow.push(row[i]);
+        }
+        verticalJoin.push(newRow.join(''));
+        verticalJoinReverse.push(newRow.reverse().join(''));
+    }
+    for (l of verticalJoin) {
+        if (l.includes(word)) return true
+    }
+    for (l of verticalJoinReverse) {
+        if (l.includes(word)) return true
+    }
+    return false;
 }
 
 module.exports = wordSearch
